e5a9d85839f14d833a7a9419d82a67ed4d1c6a22
Chris Toph <36116606+TophC7@users.noreply.github.com>
2022-04-29 20:13:21 -0400

    Created Edu Page with sample University info.

---
+++ src/lib/ascii/upr.svelte
@@ -0,0 +1,14 @@
+<p style="font: 1rem monospace; line-height:1rem;" class=" flex font-Inter text-4xl text-offWhite max-h-min max-w-minm place-items-center">
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;███████████████████▀▀&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br
+	/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;█████████████▀▀&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;▄▄▄█████▄&nbsp;&nbsp;<br
+	/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;███████▀▀&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;▐███&nbsp;&nbsp;&nbsp;████&nbsp;&nbsp;<br
+	/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;▀▀▀&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;▄▄▄█████▄&nbsp;▐███&nbsp;&nbsp;&nbsp;█▀&nbsp;&nbsp;&nbsp;&nbsp;<br
+	/>&nbsp;&nbsp;▄▄&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;███▌&nbsp;&nbsp;&nbsp;███▌&nbsp;▐████████&nbsp;&nbsp;&nbsp;&nbsp;<br
+	/>████&nbsp;&nbsp;&nbsp;&nbsp;███&nbsp;&nbsp;&nbsp;&nbsp;███▌&nbsp;&nbsp;▄██▀&nbsp;&nbsp;▐███&nbsp;&nbsp;▀███&nbsp;&nbsp;&nbsp;<br
+	/>███▌&nbsp;&nbsp;&nbsp;&nbsp;███▌&nbsp;&nbsp;&nbsp;███▌&nbsp;▀▀&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;▐███&nbsp;&nbsp;&nbsp;▀███▄▄<br
+	/>███▌&nbsp;&nbsp;&nbsp;&nbsp;███▌&nbsp;&nbsp;&nbsp;███▌&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;▀&nbsp;&nbsp;&nbsp;<br
+	/>███▌&nbsp;&nbsp;&nbsp;&nbsp;███&nbsp;&nbsp;&nbsp;&nbsp;███▌&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;▄▄&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br
+	/>████&nbsp;&nbsp;&nbsp;&nbsp;███&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;▄▄▄██████&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br
+	/>&nbsp;████▄▄▄▀▀&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;▄▄▄████████████&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br
+	/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;▄▄▄██████████████████&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />
+</p>
---
+++ src/routes/edu.svelte
@@ -0,0 +1,31 @@
+<script>
+	import UOFR from '../lib/ascii/ufor.svelte';
+	import UPR from '../lib/ascii/upr.svelte';
+	import { page } from '../lib/stores';
+
+	page.set('Education');
+</script>
+
+<div class="flex flex-col gap-14">
+	<section class=" flex gap-14">
+		<UPR />
+		<p class=" font-Inter font-semibold text-2xl text-accent">
+			University of Puerto Rico, Mayagüez <br />
+			- - - - - - - - - - - - <br />
+			Date: <span class=" font-normal text-offWhite">August 2017 - December 2021</span> <br />
+			Bachelor in: <span class=" font-normal text-offWhite">Computer Science and Engineering</span> <br />
+			Relevant courses: <span class=" font-normal text-offWhite">Computer programming in Python, Advance programming in Java, Discrete Mathematics</span> <br />
+			GPA: <span class=" font-normal text-offWhite">3.45 wtih 88 completed credits</span> <br />
+		</p>
+	</section>
+	<section class=" flex gap-14">
+		<p class=" font-Inter font-semibold text-2xl text-accent">
+			University of Regina, Saskatchewan <br />
+			- - - - - - - - - - - - <br />
+			Exchange Year: <span class=" font-normal text-offWhite"> August 2018 - May 2019</span> <br />
+			Studies in: <span class=" font-normal text-offWhite">Computer Science and Sociohumanistics</span> <br />
+			Relevant courses: <span class=" font-normal text-offWhite">Advance programming in C++, Algorithms</span> <br />
+		</p>
+		<UOFR />
+	</section>
+</div>
