1a634d34bc45740b7db3d2b60f2a26a253b54703
Chris Toph <36116606+TophC7@users.noreply.github.com>
2022-05-14 13:39:04 -0400

    Testing courier new to fix bug in mobile

--- src/lib/ascii/upr.svelte
+++ src/lib/ascii/upr.svelte
@@ -1,4 +1,4 @@
-<p style="font: 1vmin courier; line-height:1vmin;" class=" flex text-offWhite max-h-min max-w-min place-items-center">
+<p style="font: 1vmin 'courier new'; line-height:1vmin;" class=" flex text-offWhite max-h-min max-w-min place-items-center">
 	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██████████████████████&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />
 	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██████████████████████&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />
 	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;███████████████████▀▀▀&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />
