d9ff385bc5f59ad21211ea3589275e2961934b1f
Chris Toph <36116606+TophC7@users.noreply.github.com>
2022-08-20 23:29:03 -0400

    Removed old code for fresh Start

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,8 +1,3 @@
-import { writable } from 'svelte/store';
-// !!! defaults to Home
-// INFO: Used to store value of page name and have access everywhere
-export const page = writable('Home');
-
 //!!! theme colors
 export const background = '#1A1A1A';
 export const accent = '#df5b53';
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,40 +1,18 @@
 <script>
 	import '../app.css';
-	import Header from '$lib/header.svelte';
-	import Footer from '$lib/footer.svelte';
-	import InfoIcon from '$static/infoIcon.svelte';
-	import Croissant from '$static/croissant.svelte';
-	import { background, complementary } from '$lib/stores';
+	import Footer from '../lib/footer.svelte';
+	import Header from '../lib/header.svelte';
 </script>
 
-<!-- INFO:: a weird space shows up if u zoom out on mobile so i filled it with a croissant -->
-<div class=" fixed h-screen w-screen -z-10 bg-background">
-	<i class=" hover:animate-spin flex w-full h-[6vmin] fixed bottom-6 justify-center place-items-center">
-		<Croissant color={complementary} opacity=".5" />
-	</i>
-</div>
+<Header />
 
-<!-- INFO: actual layout -->
-<div class=" bg-background w-full h-full">
-	<!-- INFO: corner traingle with info icon -->
-	<a href="/info">
-		<div name="triangle" id="triangle" class=" triangle-t-r" />
-		<span class=" w-5 h-5 fixed top-[.5rem] right-1">
-			<InfoIcon color={background} />
-		</span>
-	</a>
+<main>
+	<slot />
+</main>
 
-	<!-- INFO: main site conetent -->
-	<div class="flex flex-col w-[80vw] max-w-[80rem] h-full mx-auto gap-10 py-10">
-		<Header />
-		<main class=" h-full overflow-auto pr-2">
-			<slot />
-		</main>
-		<Footer />
-	</div>
-</div>
+<Footer />
 
-<style>
+<!-- <style>
 	/* INFO: scrollbar styling */
 	/* width */
 	::-webkit-scrollbar {
@@ -56,4 +34,4 @@
 	::-webkit-scrollbar-thumb:hover {
 		background: #555;
 	}
-</style>
+</style> -->
