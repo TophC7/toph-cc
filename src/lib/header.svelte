e1995bae42707caab4c7dc97b8b355cddd575e8a
Chris Toph <36116606+TophC7@users.noreply.github.com>
2022-10-04 22:44:15 -0400

    Fixed Contact Btn Animation

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,22 +1,30 @@
 <script>
 	import Croissant from '$lib/SVGs/croissant.svelte';
-	import { faAddressCard, faCaretDown, faEnvelope, faFile, faFilePdf } from '@fortawesome/free-solid-svg-icons';
-	import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
+	import { faAddressCard, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+	import { faGithubAlt, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 	import { accent } from '$lib/stores';
 	import Fa from 'svelte-fa';
 
 	let dropdownHidden = true;
 	function toggleDropdown() {
 		dropdownHidden = !dropdownHidden;
-		document.getElementById('contact-icons').classList.toggle('scale-x-0');
+		document.getElementById('contact-icons').classList.toggle('w-0');
 		if (!dropdownHidden) {
+			//INFO: SHOWING
 			document.getElementById('contact-btn').classList.add('bg-coral-100');
 			document.getElementById('contact-btn').classList.add('text-Blueblack-900');
 			document.getElementById('contact-icons').classList.add('bg-coral-100');
+			document.getElementById('contact-icons').classList.add('w-[168px]');
+			document.getElementById('contact-icons').classList.add('p-3');
+			document.getElementById('contact-icons').classList.add('px-4');
 		} else {
+			//INFO: HIDDING
 			document.getElementById('contact-btn').classList.remove('bg-coral-100');
 			document.getElementById('contact-btn').classList.remove('text-Blueblack-900');
 			document.getElementById('contact-icons').classList.remove('bg-coral-100');
+			document.getElementById('contact-icons').classList.remove('w-[168px]');
+			document.getElementById('contact-icons').classList.remove('p-3');
+			document.getElementById('contact-icons').classList.remove('px-4');
 		}
 	}
 </script>
@@ -28,39 +36,20 @@
 		</i>
 		Chris Toph
 		<div class="relative ml-auto flex w-full place-items-center justify-end">
-			<!-- <a href="/">Github</a> -->
-			<div
-				id="contact-icons"
-				class="origin-right scale-x-0 rounded-lg rounded-r-none bg-opacity-[85%] p-3 px-4 leading-none text-Blueblack-900 transition-all ease-linear"
-			>
-				<div class="flex w-full min-w-max space-x-3">
-					<a href="https://github.com/TophC7"><Fa icon={faGithubAlt} scale={1.1} fw /></a>
-					<a href="mailto:chris@toph.cc"><Fa icon={faEnvelope} fw /></a>
-					<a href="/resume.pdf"><Fa icon={faAddressCard} fw /></a>
+			<div class="flex h-full flex-row-reverse">
+				<button id="contact-btn" class="button z-50 rounded-l-none" on:mousedown={toggleDropdown}> Contact </button>
+				<div
+					id="contact-icons"
+					class=" h-[44px] w-0 origin-center rounded-lg rounded-r-none bg-opacity-[85%] leading-none text-Blueblack-900 transition-all ease-linear"
+				>
+					<div class="flex h-full w-full min-w-max items-center gap-3 ">
+						<a href="https://github.com/TophC7"><Fa icon={faGithubAlt} scale={1.1} fw /></a>
+						<a href="https://www.linkedin.com/in/tophc7/"><Fa icon={faLinkedin} fw /></a>
+						<a href="mailto:chris@toph.cc"><Fa icon={faEnvelope} fw /></a>
+						<a href="/resume.pdf"><Fa icon={faAddressCard} fw /></a>
+					</div>
 				</div>
 			</div>
-			<button
-				id="contact-btn"
-				class="button rounded-l-none"
-				href="/"
-				on:focus={toggleDropdown}
-				on:focusout={toggleDropdown}
-			>
-				Contact
-			</button>
 		</div>
 	</div>
 </header>
-
-<style>
-	/* #contact-icons {
-		animation: slide-out 1s ease;
-		transform-origin: right;
-	}
-
-	@keyframes slide-out {
-		100% {
-			transform: scaleX(1);
-		}
-	} */
-</style>
