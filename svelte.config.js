8d08610c8d0ed6ab4ecfba90c3ad54f203b171ff
Chris <36116606+TophC7@users.noreply.github.com>
2023-10-23 16:26:04 -0400

    Dev and Prod adapters

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
-// import { preprocessMeltUI } from '@melt-ui/pp';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from 'svelte-adapter-bun';
-// import sequence from 'svelte-sequential-preprocessor';
+import adapter from '@sveltejs/adapter-auto';
+import adapterBun from 'svelte-adapter-bun';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
@@ -13,7 +15,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: process.env.DEV === 'true' ? adapterBun() : adapter(),
 	}
 };
 
